8b042e08a0944ba28298668c3e8048719546c5ca
andrewSantanastaso <andrewsantanastaso@gmail.com>
2024-10-02 09:57:24 -0400

    server operational

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
 require('dotenv').config()
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URI)
+
+mongoose.connection.once('connected', () => {
+    console.log('connected to Mongo')
+})
+
+mongoose.connection.on('error', () => {
+    console.error(error)
+})
+
+app.listen(3000, () => {
+    console.log('app running on 3000')
+})
