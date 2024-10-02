c6a993ae3afc7bd67710691f7fc3293192d6a7c8
andrewSantanastaso <andrewsantanastaso@gmail.com>
2024-10-02 10:59:49 -0400

    create and index complete

---
+++ controllers/tracks.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const router = express.Router()
+const Track = require('../models/track')
+
+router.post('/tracks', async (req, res) => {
+    try {
+        const newTrack = await Track.create({
+            title: req.body.title,
+            artist: req.body.artist
+        })
+        res.status(201).json({ newTrack })
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+})
+
+router.get('/tracks', async (req, res) => {
+    try {
+        const allTracks = await Track.find({})
+        res.status(200).json({ allTracks })
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+})
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const cors = require('cors')
+const Track = require('./models/track')
+const trackRouter = require('./controllers/tracks')
 
 mongoose.connect(process.env.MONGODB_URI)
 
@@ -12,6 +15,11 @@ mongoose.connection.once('connected', () => {
 mongoose.connection.on('error', () => {
     console.error(error)
 })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/', trackRouter)
 
 app.listen(3000, () => {
     console.log('app running on 3000')
